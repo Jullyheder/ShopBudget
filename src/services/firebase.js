8c57a9cbf001133947075e2ef38586144c2e7362
Jullyheder Cunha <jullyheder@hotmail.com>
2023-07-19 00:04:00 -0300

    Gerando o abb e apk

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,4 +1,13 @@
 import { initializeApp } from "firebase/app";
-import firebaseConfig from "../../env";
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DB_URL,
+  projectId: process.env.PROJ_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
+};
 
 export const app = initializeApp(firebaseConfig);
